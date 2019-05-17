a451b7d048c491e478d27b2986730bc7458a682e
Michal <michal.kowalik97@gmail.com>
2019-05-17 17:18:53 +0200

    Added AbstractScreen and MenuScreen

---
+++ core/src/com/project/game/screens/MenuScreen.java
@@ -0,0 +1,9 @@
+package com.project.game.screens;
+
+import com.project.game.SpaceInvanders;
+
+public class MenuScreen extends AbstractScreen {
+    public MenuScreen(SpaceInvanders game) {
+        super(game);
+    }
+}
