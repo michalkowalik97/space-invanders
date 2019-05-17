86488d24269fb0994415ec86fd657b45ae2c6bb2
Michal <michal.kowalik97@gmail.com>
2019-05-17 22:13:32 +0200

    Added GameScreen class

---
+++ core/src/com/project/game/screens/GameScreen.java
@@ -0,0 +1,13 @@
+package com.project.game.screens;
+
+import com.project.game.SpaceInvanders;
+
+public class GameScreen extends AbstractScreen {
+    public GameScreen(SpaceInvanders game) {
+        super(game);
+        init();
+    }
+
+    private void init() {
+    }
+}
